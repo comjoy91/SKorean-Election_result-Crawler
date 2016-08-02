a743f610dce9c8eb141aa863b5d8e7ace688079c
comjoy91 <comjoy91@gmail.com>
2016-08-02 17:20:39 +0900

    Hangul encode option
    Now you can give an option for encoding Korean Hangul in your output
    file. You can choose unicode(‘\uc11c\uc6b8’) or utf-8 text(‘서울’).

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from datetime import datetime
 from crawlers import Crawler
 from utils import check_dir
 
-def print_json(filename, data):
+def print_json(filename, encoding, data):
     with open(filename, 'w') as f:
-        json.dump(data, f, indent=2)
+        json.dump(data, f, indent=2, ensure_ascii=(encoding=='unicode'))
 
-def print_csv(filename, data):
+def print_csv(filename, encoding, data):
 
     def transform(txt):
         if isinstance(txt, int):
@@ -23,7 +23,7 @@ def print_csv(filename, data):
         if isinstance(txt, list):
             txt = '||'.join(txt)
         txt = txt.replace(',', '|')
-        if isinstance(txt, str):
+        if encoding=='utf8' and isinstance(txt, str) :
             txt = txt.encode('utf8')
         return txt
 
@@ -42,10 +42,10 @@ def print_csv(filename, data):
             f.write(','.join(values))
             f.write('\n')
 
-def crawl(target, _type, nth, printer, filename, level=None):
+def crawl(target, _type, nth, printer, filename, encoding, level=None):
     crawler = Crawler(target, _type, nth, level)
     cand_list = crawler.crawl()
-    printer(filename, cand_list)
+    printer(filename, encoding, cand_list)
 
 def create_parser():
     parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
@@ -56,6 +56,11 @@ def create_parser():
     parser.add_argument('start', help="starting election id", type=float)
     parser.add_argument('end', help="ending election id", type=float,\
             nargs='?', default=None)
+    parser.add_argument('filename_time', choices=['withTime', 'withoutTime'],\
+            help="descript crawling time in filename?", nargs='?', default='withoutTime')
+    parser.add_argument('encoding', choices=['unicode', 'utf8'],\
+            help="Korean Hangul encoding", nargs='?', default='utf8')
+
     parser.add_argument('-t', '--test', dest='test', action='store_true',
             help="assign datatype to csv instead of json")
     parser.add_argument('-d', dest='directory', help="specify data directory")
@@ -66,12 +71,12 @@ def create_parser():
             help="specify level for local elections.\n"
                 "- 1st char: {p:province, m:municipality, e:education},\n"
                 "- 2nd char: {g: governor, m: member}")
-    parser.add_argument('filename_time', choices=['withTime', 'withoutTime'],\
-            help="descript crawling time in filename?", nargs='?', default='withoutTime')
+
     return parser
 
 def main(args):
     printer = print_csv if args.test else print_json
+    encoding = args.encoding
     filetype = 'csv' if args.test else 'json'
     datadir = args.directory if args.directory else '.'
     time_string = datetime.today().strftime("%Y%m%d%H%M%S")
@@ -86,7 +91,7 @@ def main(args):
                     filename = '%s/%s-%s-%s-%d-%s.%s'\
                         % (datadir, args.target, args.level, args.type, n, time_string, filetype)
                     job = gevent.spawn(crawl, target=args.target, level=args.level,\
-                        _type=args.type, nth=n, filename=filename, printer=printer)
+                        _type=args.type, nth=n, filename=filename, encoding=encoding, printer=printer)
                     jobs.append(job)
                 gevent.joinall(jobs)
             else:
@@ -95,7 +100,7 @@ def main(args):
                 filename = '%s/%s-%s-%s-%.01f-%s.%s' %\
                         (datadir, args.target, args.level, args.type, n, time_string, filetype)
                 crawl(target=args.target, level=args.level, _type=args.type, nth=n,\
-                            filename=filename, printer=printer)
+                            filename=filename, encoding=encoding, printer=printer)
         else:
             if args.end:
                 jobs = []
@@ -103,7 +108,7 @@ def main(args):
                     filename = '%s/%s-%s-%d-%s.%s'\
                             % (datadir, args.target, args.type, n, time_string, filetype)
                     job = gevent.spawn(crawl, target=args.target, _type=args.type, nth=n,\
-                            filename=filename, printer=printer)
+                            filename=filename, encoding=encoding, printer=printer)
                     jobs.append(job)
                 gevent.joinall(jobs)
             else:
@@ -111,7 +116,7 @@ def main(args):
                 filename = '%s/%s-%s-%.01f-%s.%s' %\
                         (datadir, args.target, args.type, n, time_string, filetype)
                 crawl(target=args.target, _type=args.type, nth=n,\
-                            filename=filename, printer=printer)
+                            filename=filename, encoding=encoding, printer=printer)
 
     else:
         if args.target=='local':
@@ -122,7 +127,7 @@ def main(args):
                     filename = '%s/%s-%s-%s-%d.%s'\
                         % (datadir, args.target, args.level, args.type, n, filetype)
                     job = gevent.spawn(crawl, target=args.target, level=args.level,\
-                        _type=args.type, nth=n, filename=filename, printer=printer)
+                        _type=args.type, nth=n, filename=filename, encoding=encoding, printer=printer)
                     jobs.append(job)
                 gevent.joinall(jobs)
             else:
@@ -131,7 +136,7 @@ def main(args):
                 filename = '%s/%s-%s-%s-%.01f.%s' %\
                         (datadir, args.target, args.level, args.type, n, filetype)
                 crawl(target=args.target, level=args.level, _type=args.type, nth=n,\
-                            filename=filename, printer=printer)
+                            filename=filename, encoding=encoding, printer=printer)
         else:
             if args.end:
                 jobs = []
@@ -139,7 +144,7 @@ def main(args):
                     filename = '%s/%s-%s-%d.%s'\
                             % (datadir, args.target, args.type, n, filetype)
                     job = gevent.spawn(crawl, target=args.target, _type=args.type, nth=n,\
-                            filename=filename, printer=printer)
+                            filename=filename, encoding=encoding, printer=printer)
                     jobs.append(job)
                 gevent.joinall(jobs)
             else:
@@ -147,7 +152,7 @@ def main(args):
                 filename = '%s/%s-%s-%.01f.%s' %\
                         (datadir, args.target, args.type, n, filetype)
                 crawl(target=args.target, _type=args.type, nth=n,\
-                            filename=filename, printer=printer)
+                            filename=filename, encoding=encoding, printer=printer)
 
 
     print('Data written to %s' % filename)
